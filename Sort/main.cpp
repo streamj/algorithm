c3eff1039035162e7d69333cd67703c7da5e86d3
Stream <mystream@vip.qq.com>
2016-02-29 17:48:47 +0800

    buttom up mergesort

--- Sort/main.cpp
+++ Sort/main.cpp
@@ -4,7 +4,10 @@
 #include "shellsort.h"
 #include "mergesort.h"
 #include "mergesortm.h"
+#include "mergesortbu.h"
 #include <string>
+#include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -22,36 +25,56 @@ int main(int argc, char** argv)
         87, 31, 29, 82, 88, 95, 101, 293, 100, 58, 65,7, 34, 4, 6,
         77, 33, 95, 87, 30, 27, 797, 433, 235, 18, 101, 15, 3, 1, 
         0, 73, 95, 11, 2, 71, 60 };
+    
+    int* dd = new int[50];
+    srand((unsigned)time(NULL));
+    for(int i = 0; i < 50; i++){
+        dd[i] = rand() % 100;
+    }
     show(a,8);
     selectionSort(a,8);
+    assert(isSorted(a,8));
     show(a,8);
 
     cout << endl;
 
     show(b,8);
     insertionSort(b,8);
+    assert(isSorted(b,8));
     show(b,8);
 
     cout << endl;
 
     show(c,9);
     insertionSortM(c,9);
+    assert(isSorted(c,9));
     show(c,9);
 
     cout << endl;
 
     show(aa,25);
     shellSort(aa,25);
+    assert(isSorted(aa,25));
     show(aa,25);
     
     cout << endl;
     show(bb,25);
     mergeSort(bb,25);
+    assert(isSorted(bb,25));
     show(bb,25);
 
     cout << endl;
     show(cc,50);
     mergeSortM(cc,50);
+    assert(isSorted(cc,50));
     show(cc,50);
+
+    cout << endl;
+    show(dd,50);
+    mergeSortBU(dd,50);
+    assert(isSorted(dd,50));
+    show(dd,50);
+    
+    delete [] dd;
     return 0;
 }
---
+++ Sort/mergesortbu.h
@@ -0,0 +1,28 @@
+#ifndef MERGESORTBU_H
+#define MERGESORTBU_H
+template <typename T>
+void iMerge(T a[], T aux[],int lo, int mid, int hi)
+{
+    for(int i = lo; i<= hi;i++){
+        aux[i] = a[i];
+    }
+    int i = lo,j = mid+1;
+    for(int k = lo; k <=hi;k++){
+        if(i > mid)           { a[k]= aux[j++];}
+        else if(j>hi)         { a[k]= aux[i++];}
+        else if(aux[j]<aux[i]){ a[k]= aux[j++];}
+        else                  { a[k]= aux[i++];}
+    }
+}
+
+template <typename T>
+void mergeSortBU(T a[], int n)
+{
+    T* aux = new T[n];
+    for(int sz = 1; sz < n; sz = sz + sz){
+        for(int lo = 0; lo < n-sz; lo += sz + sz){
+            iMerge(a,aux,lo, lo+sz-1, lo+sz+sz-1<n-1?lo+sz+sz-1:n-1);
+        }
+    }
+}
+#endif
